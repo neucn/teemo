1bcd43950282277c3381254f75291ac809418430
Helios <i@shangyes.net>
2020-06-25 15:07:15 +0800

    build: add auto release action

---
+++ release.sh
@@ -0,0 +1,20 @@
+# $1 directory
+function pack() {
+  cp -r ../img/ "$1/"
+  cd "$1/" || exit
+  zip -r "../$1.zip" *
+  cd ../
+}
+
+GOOS=linux go build -ldflags "-w -s" -o release/linux/teemo
+GOOS=darwin go build -ldflags "-w -s" -o release/darwin/teemo
+GOOS=windows go build -ldflags "-w -s" -o release/windows/teemo.exe
+
+GOARCH=arm64 GOOS=linux go build -ldflags "-w -s" -o release/linux_arm64/teemo
+
+cd release || exit
+
+pack "linux"
+pack "darwin"
+pack "windows"
+pack "linux_arm64"
